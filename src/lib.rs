d9ee5ce935bc9939e2c61f88b3c920fd8136c959
Douglas M. Dwyer <dougmdwyer@hotmail.com>
2024-08-28 17:19:25 -0400

    Make StandardMod fields public

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ include!(concat!(env!("OUT_DIR"), "/standard_plugins.rs"));
 /// References a precompiled, builtin mod.
 pub struct StandardMod {
     /// The name of the mod.
-    name: &'static str,
+    pub name: &'static str,
     /// The binary WASM data for the mod.
-    module: &'static [u8]
+    pub module: &'static [u8]
 }
